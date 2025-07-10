027787d5d9f38d10c42bfa87ee4d3c47703dfd5d
myWorkSite1667 <worksite1667@gmail.com>
2025-07-10 17:35:34 +0600

    second

--- nav/urls.py
+++ nav/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from nav.views import home;
+from nav.views import id_show;
 
 urlpatterns = [
-    path("home/",home)
+    path("home/",home),
+    path("home/<id>",id_show)
 ]
